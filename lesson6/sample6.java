ab79386373222739cc8412bb94608615482a1e9f
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2020-05-10 23:59:23 +0900

    [add] 6-2

--- lesson6/sample6.java
+++ lesson6/sample6.java
@@ -66,3 +66,18 @@ class Sample4
 	}
 }
 
+// while文を使う
+class Sample5
+{
+	public static void main(String[] args)
+	{
+		int i = 1;
+
+		while(i <= 5){
+			System.out.println(i + "番目の繰り返しです。");
+			i++;
+		}
+
+		System.out.println("繰り返しが終わりました。");
+	}
+}
